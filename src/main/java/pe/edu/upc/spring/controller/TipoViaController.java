312d93663604acf024e05ae084a492dfdbb575d5
MLPS28 <mercedesparedes.silva@gmail.com>
2021-11-10 23:50:29 -0500

    distrito y tipo via
    falta boton buscar de tipo via

--- src/main/java/pe/edu/upc/spring/controller/TipoViaController.java
+++ src/main/java/pe/edu/upc/spring/controller/TipoViaController.java
@@ -1,5 +1,6 @@
 package pe.edu.upc.spring.controller;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sun.el.parser.ParseException;
+
 
 import pe.edu.upc.spring.model.TipoVia;
 import pe.edu.upc.spring.service.ITipoViaService;
@@ -94,5 +96,31 @@ public class TipoViaController {
 		model.put("listaTiposVias", tService.listar());
 		return "listTipoVia";
 	}
+	@RequestMapping("/irBuscar")
+	public String irBuscar(Model model) 
+	{
+		model.addAttribute("tipovia", new TipoVia());
+		return "buscartipovia";
+	}	
+
+	@RequestMapping("/buscar")
 	
+	public String buscar(Map<String, Object> model, @ModelAttribute TipoVia tipovia ) 
+	throws ParseException
+	{
+		List<TipoVia> listaTipoVias;
+		
+		tipovia.setNTipoVia(tipovia.getNTipoVia()); //capturo lo que dijite en la cajita de texto
+		
+		listaTipoVias = tService.buscarTipoVia(tipovia.getNTipoVia());
+		
+		
+		if (listaTipoVias.isEmpty()) {
+			model.put("mensaje", "No existen coincidencias");
+		}
+		
+		model.put("listaTiposVias", listaTipoVias);
+		
+		return "buscartipovia";
+	}	
 }
